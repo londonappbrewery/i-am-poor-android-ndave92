e788a156262095ea5c8699196f1e0f313327aa56
David Nagy <fishbone1992@gmail.com>
2017-11-05 10:52:18 +0100

    Challenge completed.

--- app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
@@ -1,11 +1,10 @@
 package com.londonappbrewery.iampoor;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
